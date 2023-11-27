aca2803f15511c417bcd25940791922ff49c6041
shaikh7408 <135708139+shaikh7408@users.noreply.github.com>
2023-11-27 14:34:23 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,39 +1,30 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
-from Home.models import Entry
-def home(request):
-    return render(request, "Home.html")
-def show(request):
-    data = Entry.objects.all()
-    return render(request, "show.html", {'data': data})
-def send(request):
-    if request.method == 'POST':
-        ID = request.POST['id']
-        data1 = request.POST['data1']
-        data2 = request.POST['data2']
-        Entry(ID=ID, data1=data1, data2=data2).save()
-        msg = "Data Stored Successfully"
-        return render(request, "Home.html", {'msg': msg})
-    else:
-        return HttpResponse("<h1>404 - Not Found</h1>")
-def delete(request):
-    ID = request.GET['id']
-    Entry.objects.filter(ID=ID).delete()
-    return HttpResponseRedirect("show")
-def edit(request):
-    ID = request.GET['id']
-    data1 = data2 = "Not Available"
-    for data in Entry.objects.filter(ID=ID):
-        data1 = data.data1
-        data2 = data.data2
-    return render(request, "edit.html", {'ID': ID, 'data1': data1, 'data2': data2})
-def RecordEdited(request):
-    if request.method == 'POST':
-        ID = request.POST['id']
-        data1 = request.POST['data1']
-        data2 = request.POST['data2']
-        Entry.objects.filter(ID=ID).update(data1=data1, data2=data2)
-        return HttpResponseRedirect("show")
-    else:
-        return HttpResponse("<h1>404 - Not Found</h1>")
+from django.shortcuts import render
+import mysql.connector as sql
+first_name=''
+last_name=''
+sex=''
+email=''
+password=''
+def Register(request):
+    global first_name, last_name, sex, email, password
+    if request.method == "POST":
+        m = sql.connect(host="localhost", user="root", passwd="Imad", database='student')
+        cursor = m.cursor()
+        d = request.POST
+        for key, value in d.items():
+            if key == "first_name":
+                first_name = value
+            if key == "last_name":
+                last_name = value
+            if key == "sex":
+                sex = value
+            if key == "email":
+                email = value
+            if key == "password":
+                password = value
+        c = "insert into user Values('{}','{}','{}','{}','{}')".format(first_name, last_name, sex, email, password)
+        cursor.execute(c)
+        m.commit()
+    return render(request, 'Register_page.html')
 
-# Create your views here.
+
